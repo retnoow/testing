adff4340309bb91567477b92115c81475a04b6ba
retnoow <retnowati383@gmail.com>
2022-08-15 10:33:00 +0700

    implement swagger

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const apiRouter = require('./server/routes')
 const errorHandler = require('./server/middlewares/errorHandler')
 const PORT = process.env.PORT || 4000
+const swaggerUI = require('swagger-ui-express')
+const swaggerJSON = require ('./swagger.json')
 
 // middlewares
 app.use(cors())
@@ -16,6 +18,7 @@ app.use(errorHandler)
  * entrypoint for all API routes
  */
 app.use("/api", apiRouter)
+app.use("/documentation", swaggerUI.serve, swaggerUI.setup(swaggerJSON))
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`)
